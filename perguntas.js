e1b4af80f5245d0001ca514c1ab22ecb3e57f7f1
lucassiqueira0 <160140598+lucassiqueira0@users.noreply.github.com>
2024-11-18 12:03:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,32 @@
+criaCartao(
+    '',
+    'Que ano o Vasco foi fundado?',
+    '1898'
+)
+
+criaCartao(
+    '',
+    'Qual o maior titulo da história de Vasco?',
+    'Libertadores'
+)
+
+criaCartao(
+    '',
+    'Qual o maior rival do Vasco?',
+    'Flamengo'
+)
+criaCartao(
+    '',
+    'Quantos campeonatos brasileiros o Vasco tem?',
+    '4'
+)
+criaCartao(
+    '',
+    'Qual o atual camisa 10 do Vasco?',
+    'Dimitri Payet'
+)
+criaCartao(
+    '',
+    'Qual o maior idolo da história d Vasco?',
+    'Roberto Dinamite'
+)
